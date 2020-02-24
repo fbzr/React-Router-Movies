3a07be697aa8ca85bd82feb90a8e494e09e4e9ab
fbzr <fabricio.spb@gmail.com>
2020-02-24 14:21:16 -0800

    create MovieCard

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
-import Movies from './Movies/MovieList';
+import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 
 const App = () => {
@@ -15,7 +15,7 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <Route exact path='/'>
-        <Movies />
+        <MovieList />
       </Route>
       <Route path='/movies/:id'>
         <Movie />
